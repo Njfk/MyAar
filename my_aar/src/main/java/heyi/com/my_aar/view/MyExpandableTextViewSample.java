94c3f55c095bc4a9642f5dd4a680109fa8b9ec7f
heyirsj <1192306691@qq.com>
2017-11-17 09:55:20 +0800

    MyExpandableTextView demo

---
+++ my_aar/src/main/java/heyi/com/my_aar/view/MyExpandableTextViewSample.java
@@ -0,0 +1,88 @@
+package heyi.com.picking.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import heyi.com.picking.R;
+
+
+/**
+ * Created by yanru.zhang on 16/7/26.
+ * Email:yanru.zhang@renren-inc.com
+ */
+public class MyTextView extends LinearLayout {
+    private TextView mTextView;
+    private ImageView mOpenBtn;
+    private boolean isOpen = false;
+    private int foldLines = 3; //大于3行的时候折叠
+    private int lineCounts;
+
+    public MyTextView(Context context) {
+        this(context, null);
+    }
+
+    public MyTextView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public MyTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+    }
+
+    private void initView() {
+
+        lineCounts = mTextView.getLineCount();
+        if(lineCounts <= foldLines){
+            mOpenBtn.setVisibility(GONE);
+        }
+        if(isOpen && mTextView.getHeight() != lineCounts * mTextView.getLineHeight()){
+            mTextView.setHeight(mTextView.getLineHeight() * mTextView.getLineCount());
+        }else if(!isOpen && mTextView.getHeight() != foldLines * mTextView.getLineHeight()){
+            mTextView.setHeight(mTextView.getLineHeight() * foldLines);
+        }
+        mOpenBtn.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isOpen){
+                    //to close
+                    mTextView.setHeight(mTextView.getLineHeight() * foldLines);
+                    mOpenBtn.setImageResource(android.R.drawable.arrow_up_float);
+                    isOpen = false;
+
+                }else{
+                    //to open
+                    mTextView.setHeight(mTextView.getLineHeight() * mTextView.getLineCount());
+                    mOpenBtn.setImageResource(android.R.drawable.arrow_down_float);
+
+                    isOpen = true;
+
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        Log.d("zyr","onMeasure");
+        initView();
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        Log.d("zyr","onFinishInflate");
+        if(mTextView==null || mOpenBtn == null){
+            mTextView = (TextView)getChildAt(0);
+            mOpenBtn = (ImageView)getChildAt(1);
+        }
+
+    }
+}
